e44dcd84d1179f690980ea160294eaa6ca67e00f
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2023-01-29 21:19:24 -0300

    Created the header of the application
    add the component
    add the css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from './components/Header';
+
 import './global.css';
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
+    <div>
+      <Header />
     </div>
   )
 }
